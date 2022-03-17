b78d2959815714be37e4f12e72186d53ae27b5e3
Ayoub TAGHZOUT <ayoub.taghzout@goandev.net>
2022-03-17 10:03:36 +0100

    apply dark mode for widget dummy and add .gitignore

--- Dummy/index.php
+++ Dummy/index.php
@@ -66,6 +66,11 @@ try {
     if (isset($preferences['refresh_interval'])) {
         $autoRefresh = $preferences['refresh_interval'];
     }
+    $variablesThemeCSS = match ($centreon->user->theme) {
+        'light' => "Generic-theme",
+        'dark' => "Centreon-Dark",
+        default => throw new Exception('Unknown user theme : ' . $centreon->user->theme),
+    };
 } catch (Exception $e) {
     echo $e->getMessage() . "<br/>";
     exit;
@@ -92,4 +97,5 @@ while ($row = $res->fetchRow()) {
 $template->assign('widgetId', $widgetId);
 $template->assign('autoRefresh', $autoRefresh);
 $template->assign('data', $data);
+$template->assign('theme', $variablesThemeCSS);
 $template->display('dummy.ihtml');
